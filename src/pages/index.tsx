9854fb9236c63fc078738a0bbd28adf3886c6dcb
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-08 01:51:55 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 
 import { Inter } from "next/font/google";
-import Hero from "@/components/hero";
 import NavBar from "@/components/navbar";
-import Conceito from "@/components/conceito";
-import Produtos from "@/components/produtos";
-import Roadmap from "@/components/roadmap";
 import FullPage from "@/components/fullpage";
 import Footer from "@/components/footer";
 
