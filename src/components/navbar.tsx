04fdbd842ed43debbdc22ae7d85d1d7b267e0080
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-08 01:51:23 -0300

    erros

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -53,10 +53,12 @@ export default function NavBar() {
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
-                            <img
+                            <Image
                                 className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                src="/"
                                 alt=""
+                                width={300}
+                                height={70}
                             />
                         </a>
                         <button
