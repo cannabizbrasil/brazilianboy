39008f8c2dd1552260dc7d6b323df8632eb18269
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-09 09:52:38 -0300

    Update faq.tsx

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -1,6 +1,6 @@
 import { Disclosure } from '@headlessui/react'
 import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
-import PathSvg from '../pathSvg'
+
 
 const faqs = [
     {
