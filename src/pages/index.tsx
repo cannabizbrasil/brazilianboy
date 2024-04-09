ab35d7f7603c79d7e78566ff4f3ebd598c92383d
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-09 10:02:37 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,18 +17,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 
 export default function Home() {
-  const [isOver18, setIsOver18] = useState<boolean | null>(null); // Defina o tipo de estado como booleano ou nulo
+  const [isOver18, setIsOver18] = useState<boolean | null>(null);
 
-  const handleConfirmation = (confirmed: boolean) => { // Adicione um tipo explícito para o parâmetro confirmed
+  const handleConfirmation = (confirmed: boolean) => {
     setIsOver18(confirmed);
     if (!confirmed) {
-      // Redirecione para outra página caso o usuário clique em "Não"
       window.location.href = 'https://www.youtube.com/kids/';
     }
   };
   return (
     <main>
-      {isOver18 === null && <AgeConfirmationModal onConfirm={handleConfirmation} />}
+      {isOver18 === null && <AgeConfirmationModal onConfirm={handleConfirmation} isOver18={isOver18} />}
       {isOver18 === true}
       {isOver18 === false}
       <NavBar />
