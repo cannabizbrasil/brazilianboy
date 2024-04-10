602cbca2e92ca70ae52609d0562a2c19e32beacc
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-10 14:43:00 -0300

    Update fullpage.tsx

--- src/components/fullpage.tsx
+++ src/components/fullpage.tsx
@@ -101,7 +101,7 @@ export default function FullPage() {
 
               <div className="mt-24 sm:mt-32 lg:mt-16">
                 <a href="#" className="inline-flex space-x-6">
-                  <span className="rounded-full bg-emerald-500/10 px-3 py-1 text-sm font-semibold leading-6 text-purple-500 ring-1 ring-inset ring-gray-500/20">
+                  <span className="rounded-full bg-emerald-500/10 px-6 py-1 text-sm font-semibold leading-6 text-purple-500 ring-1 ring-inset ring-gray-500/20">
                     Produtos a base de Cannabis no Brasil só podem ser vendidos com prescição médica
                   </span>
                 </a>
