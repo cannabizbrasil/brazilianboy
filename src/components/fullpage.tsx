64530b518dc07c18520d87aaa50c1c55baf5aced
MekkaLabs <153198782+cannabizbrasil@users.noreply.github.com>
2024-04-17 16:32:57 -0300

    Update fullpage.tsx

--- src/components/fullpage.tsx
+++ src/components/fullpage.tsx
@@ -85,21 +85,10 @@ export default function FullPage() {
       <main>
 
         {/* Hero section */}
-        <div>
-          <div
-            className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-            aria-hidden="true"
-          >
-            <div
-              className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#7cad24] to-[#9089fc] opacity-20"
-              style={{
-                clipPath:
-                  'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
-              }}
-            />
-          </div>
+        <div className="">
 
-          <div className="mx-auto max-w-7xl px-8 py-32">
+
+          <div className="mx-auto max-w-7xl px-8 py-12 lg:py-32">
             <div className="grid grid-cols-1 lg:grid-cols-2">
               <div>
                 <div className="mt-12 sm:mt-32 lg:mt-16">
@@ -115,7 +104,7 @@ export default function FullPage() {
                 <p className="mt-6 text-3xl leading-8 tracking-widest text-verde">
                   Lifestyle Brasileiro, qualidade Internacional.
                 </p>
-                <div className="mt-10 flex items-center gap-x-6">
+                <div className="mt-10 mb-10 flex items-center gap-x-6">
                   <ButtonVerde href="#iniciar" textobotao='Inicie sua Jornada' target='' />
                   <Link href="#jornada" className="text-sm font-semibold leading-6 text-white">
                     Etapas <span aria-hidden="true">→</span>
